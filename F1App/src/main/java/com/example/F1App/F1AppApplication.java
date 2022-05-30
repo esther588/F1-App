eb6ae5eb179d365cfb0a4f9b78d04bce3b69c4fd
Esther Cheng <esther588@berkeley.edu>
2022-05-29 23:40:30 -0700

    create project

---
+++ F1App/src/main/java/com/example/F1App/F1AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.F1App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1AppApplication.class, args);
+	}
+
+}
