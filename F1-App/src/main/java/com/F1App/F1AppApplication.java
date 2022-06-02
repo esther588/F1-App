0e114d82dae31d43298f54d920b8491b023955f5
Esther Cheng <esther588@berkeley.edu>
2022-06-01 21:42:04 -0700

    update

--- F1-App/src/main/java/com/F1App/F1AppApplication.java
+++ F1-App/src/main/java/com/F1App/F1AppApplication.java
@@ -1,4 +1,4 @@
-package com.example.F1App;
+package com.F1App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class F1AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(F1AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(F1AppApplication.class, args);
+    }
 
 }
