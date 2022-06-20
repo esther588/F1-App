8849c5a4c6264cadbea73e59ca82007e1aed3222
Esther Cheng <esther588@berkeley.edu>
2022-06-19 23:47:04 -0700

    remove age var

--- F1-App/src/main/java/com/F1App/User.java
+++ F1-App/src/main/java/com/F1App/User.java
@@ -7,15 +7,13 @@ public class User {
     private String lastName;
     private String email;
     private String password;
-    private int age;
 
-    public User(Long id, String firstName, String lastName, String email, String password, int age) {
+    public User(Long id, String firstName, String lastName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.age = age;
     }
 
     public Long getId() {
@@ -56,13 +54,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
