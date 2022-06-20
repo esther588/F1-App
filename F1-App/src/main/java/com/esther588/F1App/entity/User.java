9e91e8c12b140d88123f83242804d7c0545656c2
Esther Cheng <esther588@berkeley.edu>
2022-06-20 00:25:47 -0700

    create user class and add import statements

---
+++ F1-App/src/main/java/com/esther588/F1App/entity/User.java
@@ -0,0 +1,16 @@
+package com.esther588.F1App.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.Collections;
+
+public class User {
+}
