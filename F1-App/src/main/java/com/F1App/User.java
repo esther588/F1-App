1d9f2b4d0d7a3f6b21ee0137782cca9f54401c33
Esther Cheng <esther588@berkeley.edu>
2022-06-19 23:30:10 -0700

    add email and password

--- F1-App/src/main/java/com/F1App/User.java
+++ F1-App/src/main/java/com/F1App/User.java
@@ -5,12 +5,16 @@ public class User {
     private String firstName;
     private String lastName;
     private String user;
+    private String email;
+    private String password;
     private int age;
 
-    public User(String firstName, String lastName, String user, int age) {
+    public User(String firstName, String lastName, String user, String email, String password, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.user = user;
+        this.email = email;
+        this.password = password;
         this.age = age;
     }
 
@@ -36,6 +40,22 @@ public class User {
 
     public void setUser(String user) {
         this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public int getAge() {
