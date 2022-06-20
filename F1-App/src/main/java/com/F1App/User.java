4d44df987d74bfe53f11d3237c27dab0be8f3a38
Esther Cheng <esther588@berkeley.edu>
2022-06-19 23:25:55 -0700

    add first and last name

--- F1-App/src/main/java/com/F1App/User.java
+++ F1-App/src/main/java/com/F1App/User.java
@@ -2,22 +2,32 @@ package com.F1App;
 
 public class User {
 
-    private String name;
+    private String firstName;
+    private String lastName;
     private String id;
     private int age;
 
-    public User(String name, String id, int age) {
-        this.name = name;
+    public User(String firstName, String lastName, String id, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.id = id;
         this.age = age;
     }
 
-    public String getName() {
-        return name;
+    public String getFirst() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirst(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLast() {
+        return lastName;
+    }
+
+    public void setLast(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getId() {
