366042202b7d4f5209ebddb212f82393f53da4a3
Esther Cheng <esther588@berkeley.edu>
2022-06-20 21:59:39 -0700

    create user repo interface

---
+++ F1-App/src/main/java/com/esther588/F1App/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.esther588.F1App.user;
+
+import com.esther588.F1App.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
