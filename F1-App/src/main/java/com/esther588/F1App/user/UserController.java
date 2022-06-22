33f153804cce381273cf1b7dc4426b71ef7c5ea7
Esther Cheng <esther588@berkeley.edu>
2022-06-21 20:42:37 -0700

    add import statements

--- F1-App/src/main/java/com/esther588/F1App/user/UserController.java
+++ F1-App/src/main/java/com/esther588/F1App/user/UserController.java
@@ -1,4 +1,13 @@
 package com.esther588.F1App.user;
 
+import com.esther588.F1App.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
 public class UserController {
 }
