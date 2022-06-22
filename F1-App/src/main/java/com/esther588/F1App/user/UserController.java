0797e6d5ff1d71d65b154a4ab97785d185b415ba
Esther Cheng <esther588@berkeley.edu>
2022-06-22 00:07:38 -0700

    change from controller to rest controller

--- F1-App/src/main/java/com/esther588/F1App/user/UserController.java
+++ F1-App/src/main/java/com/esther588/F1App/user/UserController.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
