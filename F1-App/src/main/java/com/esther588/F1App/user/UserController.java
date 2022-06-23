f451b44d365c8978418347f3bd477d6c6a3a57dc
Esther Cheng <esther588@berkeley.edu>
2022-06-22 21:39:43 -0700

    remove unused imports

--- F1-App/src/main/java/com/esther588/F1App/user/UserController.java
+++ F1-App/src/main/java/com/esther588/F1App/user/UserController.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
--- F1-App/src/main/java/com/esther588/F1App/user/UserRepository.java
+++ F1-App/src/main/java/com/esther588/F1App/user/UserRepository.java
@@ -4,8 +4,6 @@ import com.esther588.F1App.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 interface UserRepository extends CrudRepository<User, Long> {
 	User findByUsername(String username);
