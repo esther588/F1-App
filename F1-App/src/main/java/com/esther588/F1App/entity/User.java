79733ee2deed86b43c9b96cee14720bbcb2ba9e1
Esther Cheng <esther588@berkeley.edu>
2022-06-25 16:05:41 -0700

    delete email

--- F1-App/src/main/java/com/esther588/F1App/entity/User.java
+++ F1-App/src/main/java/com/esther588/F1App/entity/User.java
@@ -27,7 +27,6 @@ public class User implements UserDetails {
 	private String firstName;
 	private String lastName;
 	private String username;
-	private String email;
 	private String password;
 
 	@Builder.Default
