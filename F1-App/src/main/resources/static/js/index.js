b03c1cf7d020feea79a580b8ab10e2439323a238
Esther Cheng <esther588@berkeley.edu>
2022-06-26 17:05:29 -0700

    allow js files

--- F1-App/src/main/resources/static/js/index.js
+++ F1-App/src/main/resources/static/js/index.js
@@ -1,17 +1,4 @@
-const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
-
-loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
-});
 
 signupBtn.addEventListener('click', (e) => {
 	let parent = e.target.parentNode;
