2a6cdae6630c840edcf5fba5b87cc1408587bc56
Esther Cheng <esther588@berkeley.edu>
2022-06-28 23:56:56 -0700

    edit maven file

--- F1-App/src/main/java/com/esther588/F1App/entity/User.java
+++ F1-App/src/main/java/com/esther588/F1App/entity/User.java
@@ -26,6 +26,7 @@ public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String firstName;
 	private String lastName;
 	private String username;
