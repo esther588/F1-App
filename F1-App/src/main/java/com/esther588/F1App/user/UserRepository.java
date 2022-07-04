586866d1eeb800cb65c44a62a4a0dcb83b7b8804
Esther Cheng <esther588@berkeley.edu>
2022-07-04 00:02:45 -0700

    change to jpa repo

--- F1-App/src/main/java/com/esther588/F1App/user/UserRepository.java
+++ F1-App/src/main/java/com/esther588/F1App/user/UserRepository.java
@@ -1,12 +1,12 @@
 package com.esther588.F1App.user;
 
 import com.esther588.F1App.entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-interface UserRepository extends CrudRepository<User, Long> {
+interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
 }
