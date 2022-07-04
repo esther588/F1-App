4bc34c3921d984ec38339ef73d812a19739ab84c
Esther Cheng <esther588@berkeley.edu>
2022-07-04 00:51:40 -0700

    add variables

--- F1-App/src/main/java/com/esther588/F1App/entity/Role.java
+++ F1-App/src/main/java/com/esther588/F1App/entity/Role.java
@@ -1,4 +1,22 @@
 package com.esther588.F1App.entity;
 
+import lombok.*;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    
+    @ManyToMany(mappedBy = "Roles")
+    private Set<User> users;
 }
