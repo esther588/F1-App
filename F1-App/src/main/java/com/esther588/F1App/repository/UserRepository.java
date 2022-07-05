f838ec14a833eafe5f0d41cf344be9916c278e55
Esther Cheng <esther588@berkeley.edu>
2022-07-04 21:52:54 -0700

    create new user repo

--- F1-App/src/main/java/com/esther588/F1App/repository/UserRepository.java
+++ F1-App/src/main/java/com/esther588/F1App/repository/UserRepository.java
@@ -1,10 +1,11 @@
-package com.esther588.F1App.user;
+package com.esther588.F1App.repository;
 
 import com.esther588.F1App.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface UserRepository extends JpaRepository<User, Long> {
-	User findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
+
