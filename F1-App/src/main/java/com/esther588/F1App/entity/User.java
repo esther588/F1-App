97c9c483bc8cafe588985203f11d6ba44d33987a
Esther Cheng <esther588@berkeley.edu>
2022-07-06 23:01:06 -0700

    reformat

--- F1-App/src/main/java/com/esther588/F1App/entity/User.java
+++ F1-App/src/main/java/com/esther588/F1App/entity/User.java
@@ -23,7 +23,7 @@ public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id")
+	@Column(name = "id")
 	private Long id;
 
 	@NotBlank(message = "First name cannot be blank.")
