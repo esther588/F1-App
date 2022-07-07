0311ab7fbdc49a6ccafff1fb8ead0fc199639be9
Esther Cheng <esther588@berkeley.edu>
2022-07-06 22:16:04 -0700

    edit sign up method

--- F1-App/src/main/java/com/esther588/F1App/user/UserController.java
+++ F1-App/src/main/java/com/esther588/F1App/user/UserController.java
@@ -22,9 +22,13 @@ public class UserController {
 	}
 
 	@PostMapping("/sign-up")
-	public String signUp(User user) {
-		userService.signUpUser(user);
-		return "redirect:/log-in";
+	public String signUp(@Valid User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "sign-up";
+		} else {
+			userService.signUpUser(user);
+			return "redirect:/log-in";
+		}
 	}
 
 	@GetMapping("/log-in")
