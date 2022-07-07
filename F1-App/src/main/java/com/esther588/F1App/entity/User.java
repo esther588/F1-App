adf1d4510eb96ceeb176c386448e1494c161f714
Esther Cheng <esther588@berkeley.edu>
2022-07-06 22:26:27 -0700

    add column for id

--- F1-App/src/main/java/com/esther588/F1App/entity/User.java
+++ F1-App/src/main/java/com/esther588/F1App/entity/User.java
@@ -23,6 +23,7 @@ public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private Long id;
 
 	@NotBlank(message = "First name cannot be blank.")
