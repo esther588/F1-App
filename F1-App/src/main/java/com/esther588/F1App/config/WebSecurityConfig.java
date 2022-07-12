7bf1af13ad62148580c3e8a6200f14b248d32ebf
Esther Cheng <esther588@berkeley.edu>
2022-07-11 23:12:58 -0700

    add default success url

--- F1-App/src/main/java/com/esther588/F1App/config/WebSecurityConfig.java
+++ F1-App/src/main/java/com/esther588/F1App/config/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.authenticated()
 				.and()
 				.formLogin()
+				.defaultSuccessUrl("/homepage", true)
 				.loginPage("/log-in")
 				.permitAll();
 	}
